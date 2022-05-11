41e692d16f2b9a3eea82963b510a9c1fa2546a96
mateusleviDev <mateuslevisouzapereira@gmail.com>
2022-05-10 21:35:43 -0300

    atualizando

--- arrays/script.js
+++ arrays/script.js
@@ -1,7 +1,47 @@
-const lista = ["Java", "HTML", "CSS"];
+/*const lista = ["Java", "HTML", "CSS"];
 
-const body = document.querySelector("body");
+const body = document.querySelector("body");*/
 
-for (let index = 0; index < lista.length; index++) {
+
+
+/*for (let index = 0; index < lista.length; index++) {
     body.innerHTML += "<li>" + lista[index] + "</li";
+}*/
+
+
+//método mais simples abaixo (ForEach)
+
+/*function adcionarBody(item) {
+    body.innerHTML += "<li>" + item +  "</li>"
 }
+
+lista.forEach(adcionarBody);*/
+
+const links = document.querySelectorAll("nav a");
+
+function logHref(item) {
+    const href = item.href;
+    console.log(href);
+}
+
+links.forEach(logHref);
+
+const lista = ["JavaScript", "HTML5", "CSS3"];
+
+//remove o último
+lista.pop();
+
+console.log(lista);
+
+//remove o primeiro 
+lista.shift();
+
+console.log(lista);
+
+//acrescenta 
+lista.push("PHP");
+
+console.log(lista);
+
+
+
