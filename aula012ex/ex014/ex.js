0b1068b205d099eacd191bc9433f1170c825c47c
mateusleviDev <mateuslevisouzapereira@gmail.com>
2022-06-27 20:21:39 -0300

    JavaScript.

---
+++ aula012ex/ex014/ex.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var msg = document.querySelector('#msg')
+    var img = document.querySelector('#imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'img/manha.jpg'
+        document.body.style.background = '#B0A9A2'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'img/tarde.jpg'
+        document.body.style.background = '#473654'
+    } else {
+        img.src = 'img/noite.jpg'
+        document.body.style.background = '#022B36'
+    }
+}
